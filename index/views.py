7b3dd20c3026c83ce3655eed9b31f234ae64d6b1
wubingle <bingle.wu@drgou.com>
2021-11-12 18:01:39 +0800

    模板学习

--- index/views.py
+++ index/views.py
@@ -15,5 +15,30 @@ from django.shortcuts import render  # 导入render 方法
 # 方式二
 
 
+# def test_html(request):
+#     return render(request, 'test.html', {'name': 'koko'})
+
 def test_html(request):
-    return render(request, 'test.html', {'name': 'koko'})
+    # a = {
+    #     'name': 'koko',
+    #     'course': ["Python", "C", "C++", "Java"],
+    #     'a': {'name': 'C语言中文网', 'address': 'http://c.biancheng.net/'},
+    #     'test_hello': test_hello('haha'),
+    #     'class_obj': Website()
+    # }
+    name = 'koko'
+    course = ["Python", "C", "C++", "Java"]
+    a = {'name': 'C语言中文网', 'address': 'http://c.biancheng.net/'}
+    test_hello = test_hello2()
+    class_obj = Website()
+    return render(request, 'test.html', locals())
+
+
+def test_hello2():
+    return '欢迎来到C语言中文网'
+
+
+class Website:
+    def Web_name(self):
+        return 'Hello，C语言中文网!'
+    # Web_name.alters_data = True  # 不让Website()方法被模板调用
